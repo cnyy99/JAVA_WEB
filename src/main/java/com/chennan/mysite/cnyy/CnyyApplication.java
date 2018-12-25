174c0b1bf71fce2a9a62f1034450ed7e79fe22bc
chennan <695327125@qq.com>
2018-12-25 22:54:13 +0800

    try count 1

--- src/main/java/com/chennan/mysite/cnyy/CnyyApplication.java
+++ src/main/java/com/chennan/mysite/cnyy/CnyyApplication.java
@@ -6,6 +6,8 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -14,9 +16,13 @@ import tk.mybatis.spring.annotation.MapperScan;
 //@EnableCaching
 @SpringBootApplication
 @MapperScan("com.chennan.mysite.cnyy.mybatis.mapper")
-public class CnyyApplication {
+public class CnyyApplication  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(CnyyApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CnyyApplication.class);
     }
 
     @Bean
