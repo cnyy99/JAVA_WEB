83ca53fbf709da4e0570e4aa76d5b91f569c7849
chennan <695327125@qq.com>
2018-12-28 17:41:38 +0800

    拦截相关

--- src/main/java/com/chennan/mysite/cnyy/controller/WebSecurityConfig.java
+++ src/main/java/com/chennan/mysite/cnyy/controller/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -16,42 +15,43 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  *
  */
 @Configuration
-public class WebSecurityConfig implements WebMvcConfigurer {
+public class WebSecurityConfig extends WebMvcConfigurerAdapter {
 
     /**
      * 登录session key
      */
     public final static String SESSION_KEY = "user";
 
-//    @Bean
-//    public SecurityInterceptor getSecurityInterceptor() {
-//        return new SecurityInterceptor();
-//    }
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
-//
-//        // 排除配置
-//        addInterceptor.excludePathPatterns("/error");
-//        addInterceptor.excludePathPatterns("/login**");
-//
-//        // 拦截配置
-////        addInterceptor.addPathPatterns("/tryintercept");
-//    }
-//
-//    private class SecurityInterceptor extends HandlerInterceptorAdapter {
-//
-//        @Override
-//        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//                throws Exception {
-//            HttpSession session = request.getSession();
-//            if (session.getAttribute(SESSION_KEY) != null)
-//                return true;
-//
-//            // 跳转登录
-////            String url = "/login";
-////            response.sendRedirect(url);
-//            return false;
-//        }
-//    }
+    @Bean
+    public SecurityInterceptor getSecurityInterceptor() {
+        return new SecurityInterceptor();
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
+
+        // 排除配置
+        addInterceptor.excludePathPatterns("/error");
+        addInterceptor.excludePathPatterns("/login**");
+        addInterceptor.excludePathPatterns("//addlogin");
+
+        // 拦截配置
+        //addInterceptor.addPathPatterns("/**");
+    }
+
+    private class SecurityInterceptor extends HandlerInterceptorAdapter {
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+                throws Exception {
+            HttpSession session = request.getSession();
+            if (session.getAttribute(SESSION_KEY) != null)
+                return true;
+
+            // 跳转登录
+            String url = "/login";
+            response.sendRedirect(url);
+            return false;
+        }
+    }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -3,14 +3,69 @@
 <html xmlns:th="http://www.thymeleaf.org">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>玩转spring boot——简单登录认证</title>
+    <title>玩转——简单登录认证</title>
 </head>
 <body>
-<h1>玩转spring boot——简单登录认证</h1>
+<h1>玩转——简单登录认证</h1>
 <h3>登录用户： ${username}</h3>
 
 <a href="/logout">注销</a>
 <br />
 <a href="http://www.cnblogs.com/GoodHelper/"></a>
+<el-row>
+    <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+</el-row>
+<el-row>
+    <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+</el-row>
+<el-row>
+    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+</el-row>
+<el-row>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+</el-row>
+<el-row>
+    <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+    <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+    <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+</el-row>
+
+<style lang="scss">
+    .el-row {
+        margin-bottom: 20px;
+    &:last-child {
+         margin-bottom: 0;
+     }
+    }
+    .el-col {
+        border-radius: 4px;
+    }
+    .bg-purple-dark {
+        background: #99a9bf;
+    }
+    .bg-purple {
+        background: #d3dce6;
+    }
+    .bg-purple-light {
+        background: #e5e9f2;
+    }
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+    .row-bg {
+        padding: 10px 0;
+        background-color: #f9fafc;
+    }
+</style>
 </body>
 </html>
