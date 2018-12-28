9df8bd8e735942c7cd417a50444985d47e30d872
chennan <695327125@qq.com>
2018-12-28 21:17:53 +0800

    remove extra '/'

--- src/main/java/com/chennan/mysite/cnyy/controller/WebSecurityConfig.java
+++ src/main/java/com/chennan/mysite/cnyy/controller/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter {
         // 排除配置
         addInterceptor.excludePathPatterns("/error");
         addInterceptor.excludePathPatterns("/login**");
-        addInterceptor.excludePathPatterns("//addlogin");
+        addInterceptor.excludePathPatterns("/addlogin");
 
         // 拦截配置
         //addInterceptor.addPathPatterns("/**");
